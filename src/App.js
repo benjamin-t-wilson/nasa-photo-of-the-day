9698116cda86a5691771e5be8037b6cbe0406abe
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-07-30 18:57:04 -0700

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import APOD from "./components/APOD";
+import Headline from "./components/Headline";
 
 function App() {
+  const [nasaPic, setNasaPic] = useState({});
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(response => {
+        setNasaPic(response.data);
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
+    <>
+      <Headline />
+      <APOD
+        title={nasaPic.title}
+        date={nasaPic.date}
+        url={nasaPic.url}
+        explanation={nasaPic.explanation}
+        copyright={nasaPic.copyright}
+      />
+    </>
   );
 }
 
